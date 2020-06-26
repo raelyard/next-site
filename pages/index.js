cf1e1982b6ba2db8cd50649e4da22dcb37a8db8b
Dave Rael <dave@raelyard.com>
2020-06-26 10:50:18 -0600

    Bare-bones site
    just to get something up and running

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>asdf</title>
+      </Head>
+      <section>
+        <p>Hello</p>
+      </section>
+    </>
+  )
+}
